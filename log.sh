15294374a670c673f20b2889da13ee145118c1e6
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-02-25 00:20:35 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -19,46 +19,6 @@ if [ "$MYIP" = "" ]; then
 fi
 #MYIP=$(wget -qO- ipv4.icanhazip.com)
 clear
-echo " "
-if [ -e "/var/log/auth.log" ]; then
-        LOG="/var/log/auth.log";
-fi
-if [ -e "/var/log/secure" ]; then
-        LOG="/var/log/secure";
-fi
-data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
-echo "---------=[ Checking Dropbear Login ]=---------"
-echo "==============================================="
-echo "|   PID    |   Username    |    IP Address    |"
-echo "-----------------------------------------------"
-cat $LOG | grep -i dropbear | grep -i "Password auth succeeded" > /tmp/login-db.txt;
-for PID in "${data[@]}"
-do
-        cat /tmp/login-db.txt | grep "dropbear\[$PID\]" > /tmp/login-db-pid.txt;
-        NUM=`cat /tmp/login-db-pid.txt | wc -l`;
-        USER=`cat /tmp/login-db-pid.txt | awk '{print $10}'`;
-        IP=`cat /tmp/login-db-pid.txt | awk '{print $12}'`;
-        if [ $NUM -eq 1 ]; then
-                echo "$PID - $USER - $IP";
-                fi
-done
-echo " "
-echo "---------=[ Checking OpenSSH Login ]=---------"
-echo "==============================================="
-echo "|   PID    |   Username    |    IP Address    |"
-echo "-----------------------------------------------"
-cat $LOG | grep -i sshd | grep -i "Accepted password for" > /tmp/login-db.txt
-data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
-for PID in "${data[@]}"
-do
-        cat /tmp/login-db.txt | grep "sshd\[$PID\]" > /tmp/login-db-pid.txt;
-        NUM=`cat /tmp/login-db-pid.txt | wc -l`;
-        USER=`cat /tmp/login-db-pid.txt | awk '{print $9}'`;
-        IP=`cat /tmp/login-db-pid.txt | awk '{print $11}'`;
-        if [ $NUM -eq 1 ]; then
-                echo "$PID - $USER - $IP";
-        fi
-done
 echo ""
 if [ -f "/var/log/openvpn/status.log" ]; then
         line=`cat /var/log/openvpn/status.log | wc -l`
