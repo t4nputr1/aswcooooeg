74987992e1837ffedeaf0b6a480cd76ee7448508
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-02-27 01:03:48 +0700

    Update pptp.sh

--- pptp.sh
+++ pptp.sh
@@ -4,18 +4,11 @@ if [[ $USER != 'root' ]]; then
 	echo "Maaf, Anda harus menjalankan ini sebagai root"
 	exit
 fi
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
-#MYIP=$(wget -qO- ipv4.icanhazip.com)
+MYIP=$(wget -qO- ipv4.icanhazip.com)
 
 u="abcde"
 p="abcde"
 
-# get the VPS IP
-#ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
-ip=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
-if [ "$ip" = "" ]; then
-	ip=$(wget -qO- ipv4.icanhazip.com)
-fi
 
 echo
 echo "######################################################"
@@ -89,9 +82,6 @@ sleep 5
 echo
 echo "######################################################"
 echo "Server setup complete!"
-echo "Connect to your VPS at $ip with these credentials:"
+echo "Connect to your VPS at $MYIP with these credentials:"
 echo "Username:$u ##### Password: $p"
 echo "######################################################"
-
-cd ~/
-rm -f /root/IP
