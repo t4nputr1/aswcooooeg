95fc59b48c810c7895ac26412bb045145c27fcc7
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-07-26 23:53:08 +0700

    Update siti.sh

--- siti.sh
+++ siti.sh
@@ -31,7 +31,6 @@ echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/v2ray/domain
 echo "$host" >>  /root/domain
 
-wget https://raw.githubusercontent.com/lesta-1/sc/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/lesta-1/sc/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/lesta-1/sc/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
