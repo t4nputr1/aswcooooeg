2337478104b82ba4b58a1816b50ee04b270edf35
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-07-27 03:15:59 +0700

    Update siti.sh

--- siti.sh
+++ siti.sh
@@ -1,4 +1,3 @@
-cat > /usr/local/bin/edu-proxyssh <<END
 #!/usr/bin/python
 import socket, threading, thread, select, signal, sys, time, getopt
 
@@ -266,7 +265,3 @@ def main(host=LISTENING_ADDR, port=LISTENING_PORT):
 #######    parse_args(sys.argv[1:])
 if __name__ == '__main__':
     main()
-END
-
-chmod +x /usr/local/bin/edu-proxyssh
-rm -f /root/siti.sh
